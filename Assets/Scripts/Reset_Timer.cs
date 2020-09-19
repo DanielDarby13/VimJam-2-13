283a8b6fb89214c951d331158900018e3f45ec39
Alex Williamson <alex_williamson@rocketmail.com>
2020-09-19 17:31:49 +0100

    Added the ability to "Astral Project" or whatever we're calling it. Happens when you press E, player object needs to be set on the Reset_Timer script in the inspector, time before it sends you back is also configurable in the inspector.

---
+++ Assets/Scripts/Reset_Timer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset_Timer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player_Object;
+    [SerializeField]
+    private int Seconds_Until_Return;
+    private Vector3 Original_Position;
+    private bool Projecting = false;
+    private CharacterController Controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = Player_Object.GetComponent<ThirdPersonMovement>().controller;
+        Original_Position = Player_Object.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Projecting == false)
+            {
+                Projecting = true;
+                Astral_Projection();
+            }
+        }
+    }
+
+    private void Astral_Projection()
+    {
+        Original_Position = Player_Object.transform.position;
+        StartCoroutine(Projection_Clock());
+    }
+
+    IEnumerator Projection_Clock()
+    {
+        yield return new WaitForSeconds(Seconds_Until_Return);
+        Player_Object.GetComponent<ThirdPersonMovement>().Can_Move = false;
+        Controller.Move(new Vector3(0, 0, 0));
+        Player_Object.transform.position = Original_Position;
+        yield return new WaitForSeconds(0.1f);
+        Player_Object.GetComponent<ThirdPersonMovement>().Can_Move = true;
+        Projecting = false;
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -7,7 +7,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
     // script sourced from https://www.youtube.com/watch?v=4HpC--2iowE
 
-
+    public bool Can_Move = true;
     public CharacterController controller;
     public Transform cam;
 
@@ -19,18 +19,22 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if (direction.magnitude >= 0.1f)
+        print(Can_Move);
+        if (Can_Move)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
         }
     }
 }
